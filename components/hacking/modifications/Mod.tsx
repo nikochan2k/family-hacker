5f80bb9a04ac99f693ee4ac53db40df062650b55
Yoshihiro Seki <nikochan2k@gmail.com>
2022-02-23 23:54:55 +0900

    ability to remove modification

--- components/hacking/modifications/Mod.tsx
+++ components/hacking/modifications/Mod.tsx
@@ -1,35 +1,63 @@
-import { Box, HStack, Link, Text } from "native-base";
+import { Button, HStack, Link, Text } from "native-base";
 import React, { Fragment, VFC } from "react";
-import { useSetRecoilState } from "recoil";
-import { Modification, modificationAtom } from "../../../stores/modifications";
+import { useRecoilCallback, useSetRecoilState } from "recoil";
+import {
+  Modification,
+  modificationAtom,
+  modificationsAtom,
+} from "../../../stores/modifications";
 import { toHex } from "../../../util";
 
 export const Mod: VFC<{ mod: Modification }> = ({ mod }) => {
   const setModification = useSetRecoilState(modificationAtom);
 
+  const remove = useRecoilCallback(
+    ({ snapshot, set }) =>
+      async (modToRemove: Modification) => {
+        const modifications = await snapshot.getPromise(modificationsAtom);
+        const newModifications: Modification[] = [];
+        let found = false;
+        for (const m of modifications) {
+          if (m.address !== modToRemove.address) {
+            newModifications.push(m);
+          } else {
+            found = true;
+          }
+        }
+        if (found) {
+          set(modificationsAtom, newModifications);
+        }
+      },
+    []
+  );
+
   return (
-    <HStack>
-      <Box width={"40px"}>
-        <Text size="container" color="dark.300">
-          {toHex(mod.address, 4)}
-        </Text>
-      </Box>
-      <Box width={"20px"}>
-        <Link
-          onPress={() => setModification(mod)}
-          _text={{
-            color: "dark.400",
-          }}
-        >
-          {toHex(mod.value, 2)}
-        </Link>
-      </Box>
+    <HStack alignItems="center">
+      <Button
+        size="container"
+        minWidth="16px"
+        variant="ghost"
+        colorScheme="danger"
+        onPress={() => remove(mod)}
+      >
+        ×
+      </Button>
+      <Text minWidth="40px" size="container" color="dark.300">
+        {toHex(mod.address, 4)}
+      </Text>
+      <Link
+        minWidth="20px"
+        onPress={() => setModification(mod)}
+        _text={{
+          color: "dark.400",
+        }}
+      >
+        {toHex(mod.value, 2)}
+      </Link>
       {mod.name ? (
-        <Box marginLeft="10px">
-          <Text color="dark.500" italic>
-            {mod.name}
-          </Text>
-        </Box>
+        <Text marginLeft="10px" color="dark.500" italic>
+          {mod.name}
+        </Text>
       ) : (
         <Fragment />
       )}
